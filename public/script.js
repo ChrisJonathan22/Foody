877d16aa156a531bda19a06e33d8cdd7ec043cd9
Chris Jonathan <christophereko@hotmail.fr>
2019-01-10 08:55:14 +0000

    I have given the navbar a position fixed so that it's always within view, I've also added a scroll event on the window which when triggered will change the navbar's background color to black and then it'll go back to its original color once the user stops scrolling. I've also made some changes to the form section, I've added two inner sections inside it.

---
+++ public/script.js
@@ -0,0 +1,12 @@
+let navBar = document.getElementById('nav-bar');    // Access the element with an id of nav-bar.
+let timer = null;   // Create a variable and assign it to null;
+
+window.addEventListener('scroll', () => {   // When a user scrolls the page do this.
+    navBar.style.backgroundColor = "black"; // Change the background color of the navbar black.
+    if(timer !== null) {    // If timer isn't equal to null do this.
+        clearTimeout(timer);    // Clear the timeout.
+    }
+    timer = setTimeout(() => {  // Set up a timer which will be triggered after 250 ms / 0.25 sec.
+        navBar.style.backgroundColor = "rgba(10, 10, 10, 0.3)"; // Once the timer is active change the navbar's background color to its original color.
+    }, 250);
+}, false);
